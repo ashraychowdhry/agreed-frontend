69feb3063500669a9f51dcca63ba5ff56c1f76e7
Ashray Chowdhry <ashraychowdhry@gmail.com>
2021-11-16 17:42:21 -0500

    reorg and landing screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register' 
-import Dashboard from './components/Dashboard'
+import Login from './Pages/Login'
+import Register from './Pages/Register' 
+import Dashboard from './Pages/Dashboard'
+import Landing from './Pages/Landing'
+import HeaderBar from './Components/HeaderBar'
 
 const App = () => {
 	return (
 		<div>
 			<BrowserRouter>
-                <Routes> 
+                <HeaderBar />
+                <Routes>
+                    <Route path='/' element={<Landing />} /> 
                     <Route path="/login" element={<Login/>} />
                     <Route path="/register" element={<Register/>} />
                      <Route path="/dashboard" element={<Dashboard/>} />
---
+++ src/Pages/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './landing.css'
+
+export default function Landing() {
+    return (
+        <div className='landing-wrapper'>
+            <p>Hi</p>
+        </div>
+    )
+}
