55b9d973d4e21e16fd47b9318dc4e850a52ccff1
MikellePrime <70291597+MikellePrime@users.noreply.github.com>
2021-11-29 12:15:25 -0500

    add dashboard style

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import NavBar from '../Components/NavBar'
+import './dashboard.css';
 
 
 const Dashboard = () => {
 	return (
 		<div>
 			<NavBar/>
+			<div className='group'>
+				<h2>Trip Dashboard</h2>
+				<h3>Group Pin</h3>
+				
+			</div>
+
 			<Button variant="contained" color="secondary" href='/creategroupform'>Create a Group</Button>
 			<Button variant="contained" color="secondary" href='/individualform'>Individual Form</Button>
 		</div>
