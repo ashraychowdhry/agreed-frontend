0d5dc7c9aa1ab199547733179096ecbe330b59f8
GSlaw <46406693+GSlaw@users.noreply.github.com>
2021-11-30 18:33:55 -0500

    Individual Form Styling Progress

--- src/Components/IndividualFormSteps/Form1.js
+++ src/Components/IndividualFormSteps/Form1.js
@@ -3,6 +3,7 @@ import AppContext from "./AppContext"
 import Button from '@material-ui/core/Button';
 import './styles.css';
 
+
 export default function Form1() {
     const myContext = useContext(AppContext);
     const updateContext = myContext.userDetails;
@@ -20,15 +21,15 @@ export default function Form1() {
         <div className="contain">
             <p>Enter Your Details</p>
             <form className="form">
-                <div class="grid-container"> 
-                <input className="formInput" type="text" placeholder="Airline Preference" onChange={e => updateContext.setFlightPreference(e.target.value)} required/>
-                </div>
-                <div class="grid-container"> 
-                <input className="formInput" type="text" placeholder="Budget for Flight" onChange={e => updateContext.setBudget(e.target.value)} required/>
-                </div>
-                <div class="grid-container"> 
-                <Button type="button" variant ="contained" className="formSubmit" color ="primary" onClick={next} >Next</Button>
-                </div>
+                    <div> 
+                    <input className="formInput" type="text" placeholder="Airline Preference" onChange={e => updateContext.setFlightPreference(e.target.value)} required/>
+                    </div>
+                    <div> 
+                    <input className="formInput" type="text" placeholder="Budget for Flight" onChange={e => updateContext.setBudget(e.target.value)} required/>
+                    </div>
+                    <div> 
+                    <Button type="button" variant ="contained" className="formSubmit" color ="primary" onClick={next} >Next</Button>
+                    </div>
             </form>
         </div>
     );
--- src/Components/IndividualFormSteps/IndividualForm.js
+++ src/Components/IndividualFormSteps/IndividualForm.js
@@ -7,6 +7,14 @@ import Form3 from './Form3'
 import SubmitForm from './SubmitForm'
 import Progress from './Progress'
 import NavBar from '../NavBar.js';
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
+import TF from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { Content } from 'react-bootstrap/lib/Tab';
+
 
 export default function IndividualForm() {
     const [step, setStep] = useState(0);
@@ -38,19 +46,24 @@ export default function IndividualForm() {
         <AppContext.Provider value={{userDetails}}>
             <div class ="blueBG">      
             <NavBar/>
+        
             <br/>
-            <div class="menuCard"> 
-            <div className="body">
-                 <h3 class="redText" >Multi Step Individual Form</h3>
-                 <div className="wrapper">
-                 <Progress />
-                 {step === 0 && <Form1 /> }
-                 {step === 1 && <Form2 /> }
-                 {step === 2 && <Form3 /> }
-                 {step === 3 && <SubmitForm /> }
-                 </div>
-            </div>
-             </div>
+                <div class="individualFormCard"> 
+                        <h3 class="redText" >Multi Step Individual Form</h3>
+                        <div style={{paddingTop: '25px', paddingLeft: '150px'}}>
+                            <Progress />
+                            {step === 0 && <Form1 /> } 
+                            {step === 1 && <Form2 /> }
+                            {step === 2 && <Form3 /> }
+                            {step === 3 && <SubmitForm /> }
+                        </div>
+                        
+                            
+
+                            
+                        
+                    
+                </div>
             </div>
             
         </AppContext.Provider>
