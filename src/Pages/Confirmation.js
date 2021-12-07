f04094e44b9cb37cfabad54607cb556f6eb2917c
MikellePrime <70291597+MikellePrime@users.noreply.github.com>
2021-12-07 10:19:31 -0500

    cont. styling

--- src/Pages/Confirmation.js
+++ src/Pages/Confirmation.js
@@ -49,28 +49,30 @@ export default function Confirmation(props) {
 
 */
     return (
-        <div className='confirm'>
-            <NavBar/>
-            <h1 className='title'>Success! Your flights have been booked. Sit back and relax.</h1>
-            <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
-                {response.map((obj, i) => {
-                  return (
-                    <div>
+        <div className='confirmbackground'>
+            <div className='confirm'>
+                <NavBar/>
+                <h1 className='title'>Success! Your flights have been booked. Sit back and relax.</h1>
+                <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
+                    {response.map((obj, i) => {
+                      return (
+                        <div>
 
-                        <div className='group-list' style={{}} key={i}>
-                            <FlightResultCard
-                            personName={obj.userID}
-                            departs={obj.flight.itineraries[0].segments[0].departure.at}
-                            arrives={obj.flight.itineraries[0].segments[0].arrival.at}
-                            startAirport={obj.flight.itineraries[0].segments[0].departure.iataCode}
-                            endAirport={obj.flight.itineraries[0].segments[0].arrival.iataCode}
-                            flightPrice={obj.flight.price.total} />
+                            <div className='group-list' style={{}} key={i}>
+                                <FlightResultCard
+                                personName={obj.userID}
+                                departs={obj.flight.itineraries[0].segments[0].departure.at}
+                                arrives={obj.flight.itineraries[0].segments[0].arrival.at}
+                                startAirport={obj.flight.itineraries[0].segments[0].departure.iataCode}
+                                endAirport={obj.flight.itineraries[0].segments[0].arrival.iataCode}
+                                flightPrice={obj.flight.price.total} />
+                            </div>
                         </div>
-                    </div>
-                  )
-                })}
-            </div>
+                      )
+                    })}
+                </div>
 
+            </div>
         </div>
 
     )
