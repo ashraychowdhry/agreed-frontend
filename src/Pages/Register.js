881f6feff207a62080934ae875e49a7375f0c4c9
MikellePrime <70291597+MikellePrime@users.noreply.github.com>
2021-12-06 23:33:11 -0500

    style new register

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,6 +1,12 @@
-// THIS FILE IS UNUSED // 
+// THIS FILE IS UNUSED //
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import './landing.css'
+import '../styles.css'
+import './register.css'
+import TextField from '@mui/material/TextField';
+import { borders } from '@mui/system';
+import Button from '@material-ui/core/Button';
 
 function Register() {
 	const navigate = useNavigate();
@@ -41,60 +47,75 @@ function Register() {
 
 
   return (
-    <div className='registerPopup'>
-	    <h1>Register</h1>
+    <div className='registerPage'>
+		<div className='navbar'>
+			<div className='nav-left'>
+				<a href="/">ABEONA</a>
+			</div>
+			<div className='nav-right'>
+				<ul>
+					<li><a href="/dashboard">Dashboard</a></li>
+					<li><a href="/aboutus">About Us</a></li>
+
+				</ul>
+			</div>
+		</div>
+		<div className='registerPopup'>
+	    	<h1 className='title'>Register to get started.</h1>
 			<form onSubmit={registerUser}>
-				<input
+				<TextField id="outlined-basic" label="First Name" variant="outlined" style={{ minWidth: '225px', borderRadius: '50px' }}
 					value={firstName}
 					onChange={(e) => setFirstName(e.target.value)}
 					type="text"
 					placeholder="First Name"
 				/>
 				<br />
-        	    <input
+        	    <TextField id="outlined-basic" label="Last Name" variant="outlined" style={{ margin: '10px auto', minWidth: '225px', borderRadius: '50px' }}
 					value={lastName}
 					onChange={(e) => setLastName(e.target.value)}
 					type="text"
 					placeholder="Last Name"
 				/>
 				<br />
-				<input
+				<TextField id="outlined-basic" label="Email" variant="outlined" style={{ minWidth: '225px', borderRadius: '50px' }}
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 					type="email"
 					placeholder="Email"
 				/>
 				<br />
-				<input
+				<TextField id="outlined-basic" label="Password" variant="outlined" style={{ margin: '10px auto', minWidth: '225px', borderRadius: '50px' }}
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 					type="password"
 					placeholder="Password"
 				/>
 				<br />
-				<input
+				<TextField id="outlined-basic" label="Birthday" variant="outlined" style={{ minWidth: '225px', borderRadius: '50px' }}
 					value={birthday}
 					onChange={(e) => setBirthday(e.target.value)}
 					type="text"
 					placeholder="DOB MM/DD/YYYY"
 				/>
 				<br />
-				<input
+				<TextField id="outlined-basic" label="Age" variant="outlined" style={{ margin: '10px auto', minWidth: '225px', borderRadius: '50px' }}
 					value={age}
 					onChange={(e) => setAge(e.target.value)}
 					type="text"
 					placeholder="Age"
 				/>
 				<br />
-				<input
+				<TextField id="outlined-basic" label="Gender" variant="outlined" style={{ minWidth: '225px', borderRadius: '50px' }}
 					value={gender}
 					onChange={(e) => setGender(e.target.value)}
 					type="text"
 					placeholder="Gender"
 				/>
 				<br />
-				<input type="submit" value="Register" />
+				<Button variant="contained" color="secondary" type="submit" style={{ margin: '20px', minWidth: '225px', minHeight: '40px', borderRadius: '15px' }}>Register</Button>
+
 			</form>
+		</div>
 	</div>
   );
 }
